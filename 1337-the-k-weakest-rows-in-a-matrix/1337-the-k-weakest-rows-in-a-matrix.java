ba0ce717c77d3da1256520ed9299c7bff4a44aa0
Parth Saarthi Bhati <49817355+Parth4400@users.noreply.github.com>
2022-03-31 14:31:41 +0530

    Time: 2 ms (84.61%), Space: 43.4 MB (86.54%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,27 +1,46 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
-        int ans[]=new int[k];
-       int res[][]=new int[mat.length][2]; 
+     //   
+        int r=mat.length,c=mat[0].length;
         
-        for(int i=0;i<mat.length;i++)
-        {int sol=0;
-            for(int j=0;j<mat[0].length;j++)
+        PriorityQueue<int []> que=new PriorityQueue<>((a,b)-> a[0]==b[0]? b[1]-a[1]:b[0]-a[0]);
+        int sol=0;
+        int res[]=new int[k];
+        for(int i =0;i<r;i++)
+        {  sol=0;
+            for(int j=0;j<c;j++)
             {
                 if(mat[i][j]==1)
                     sol++;
             }
          
-         res[i][0]=sol;
-         res[i][1]=i;
+         if(que.size()>=k)
+         {
+//               PriorityQueue<int []> que1=new PriorityQueue<>(que);
+//              for(int p=0;p<que.size();p++)
+//                  System.out.println(Arrays.toString(que1.poll()));
+             
+//              System.out.println("---------------");
+             int ele[]=que.poll();
+       //       System.out.println(Arrays.toString(ele));
+             if(sol<ele[0])
+             {
+                 que.offer(new int[]{sol,i});
+             }
+             else
+            que.offer(ele);
+         }
+         else
+             que.offer(new int[]{sol,i});
         }
         
-        Arrays.sort(res,(a,b)-> a[0]==b[0]?a[1]-b[1]:a[0]-b[0]);
-        // for(int i=0;i<mat.length;i++)
-        // System.out.println(Arrays.toString(res[i]));
-        for(int i=0;i<k;i++)
-        {
-            ans[i]=res[i][1];
-        }
-        return ans;
+        
+    
+    for(int i=0;i<k;i++)
+    {
+res[k-i-1]=que.poll()[1];
+    }
+    
+    return res;
     }
 }
