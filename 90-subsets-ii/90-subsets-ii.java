91f498e4fd58ae3cc9f4dcd4f4ce70597c37a09e
Parth Saarthi Bhati <49817355+Parth4400@users.noreply.github.com>
2022-04-25 17:42:06 +0530

    Time: 1 ms (97.83%), Space: 42.8 MB (75.01%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void genUniqueSubset(int nums[],int index,List<Integer> curr,
+                               List<List<Integer>> res)
+    {
+        if(index>=nums.length)
+        {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        int i=index;
+        for(;i<nums.length;i++)
+        {
+            if(i==index || ((i-1>=0) && nums[i-1]!=nums[i]))
+            {
+                curr.add(nums[i]);
+                genUniqueSubset(nums,i+1,curr,res);
+                curr.remove(curr.size()-1);
+            }
+        }
+        if(i>=nums.length)
+            res.add(new ArrayList<>(curr));
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res=new ArrayList<>();
+        genUniqueSubset(nums,0,new ArrayList(),res);
+        return res;
+        
+    }
+}
