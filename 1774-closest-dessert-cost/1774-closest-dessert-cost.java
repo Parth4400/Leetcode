c7776d1ddb4d926fafd66bc658e580b93cb14743
Parth Saarthi Bhati <49817355+Parth4400@users.noreply.github.com>
2022-08-17 00:49:21 +0530

    Time: 39 ms (19.96%), Space: 41.4 MB (64.92%) - LeetHub

---
+++ 1774-closest-dessert-cost/1774-closest-dessert-cost.java
@@ -0,0 +1,36 @@
+class Solution {
+    int tot_cost[]=new int[2];
+    public void dfs(int cost[],int index,int calc,int t)
+    {
+        //System.out.println(calc);
+        int differ=Math.abs(calc-t);
+        if(differ<=tot_cost[1])
+        {
+          
+            if(differ<tot_cost[1])
+                tot_cost[0]=calc;
+            if(differ==tot_cost[1])
+            tot_cost[0]=Math.min(tot_cost[0],calc);
+            
+              tot_cost[1]=differ;
+        }
+        if(index>=cost.length)
+            return ;
+       
+        for(int i=0;i<3;i++)
+        {
+      int  top_cost=    cost[index]*i;
+            dfs(cost,index+1,calc+top_cost,t);
+        }
+    }
+    public int closestCost(int[] baseCosts, int[] toppingCosts, int target) {
+        tot_cost[0]=Integer.MAX_VALUE;
+        tot_cost[1]=Integer.MAX_VALUE;
+        for(int i=0;i<baseCosts.length;i++)
+        {
+            dfs(toppingCosts,0,baseCosts[i],target);
+        }
+       // System.out.println(tot_cost[1]);
+        return tot_cost[0];
+    }
+}
